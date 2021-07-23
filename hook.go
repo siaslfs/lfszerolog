1630e0cef17a283e519354a96295bed658ce55db
siaslfs <lifs@rp-pet.cn>
2021-07-23 10:10:42 +0800

    HOOK mysql

--- hook.go
+++ hook.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	hook := &hookers.LfsHooker{}
-	hook, _ = hook.NewMysqlHooker("root:Rp000000@(10.1.1.245:3306)/logs_db?charset=utf8mb4")
+	hook, _ = hook.NewMysqlHooker("root:root@(127.0.0.1:3306)/logs_db?charset=utf8mb4")
 	hooded := log.Hook(hook)
 	hooded.Info().Str("foo", "bar").Msg("hello world")
 	hooded.Info().Fields(map[string]interface{}{
